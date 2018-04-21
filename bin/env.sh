cff11aa6e744256f66f8bd52fff25bd877bec79f
unknown <2195496026@qq.com>
2018-04-21 23:46:51 +0800

    增加bin文件

---
+++ bin/env.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env bash
+#开始时需要配置参数，root、projectName、gitUrl、webapps、tomcatBin
+#项目源码根目录
+root="/root/project/"
+#项目名
+projectName="test"
+#github仓库地址
+gitUrl="https://github.com/cquptrhw/tsetUplaod"
+#webapps目录
+webapps="/usr/local/tomcat/webapps/"
+#tomcat服务器bin目录
+tomcatBin="/usr/local/tomcat/bin/"
